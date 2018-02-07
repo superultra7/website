a33f28b334970100b80efc5202bca85acb81d112
Roger Pettett <roger.pettett@nanoporetech.com>
2018-02-07 18:23:33 +0000

    corrected paths

---
+++ vessels/index.js
@@ -0,0 +1,6 @@
+export { default as Battleship } from "./battleship";
+export { default as Carrier    } from "./carrier";
+export { default as Destroyer  } from "./destroyer";
+export { default as Submarine  } from "./submarine";
+export { default as Frigate    } from "./frigate";
+export { default as Lifeboat   } from "./lifeboat";
